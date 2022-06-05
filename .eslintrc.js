8e745a2fd0ae574c5a73e3890cabdb4bb184876f
1sh1vam <shivam07.mt@gmail.com>
2022-06-05 14:13:43 +0530

    setting up the prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+    "prettier",
+    "plugin:react/jsx-runtime",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+  },
+};
---
+++ src/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: true,
+    tabWidth: 2,
+    printWidth: 100,
+    jsxBracketSameLine: true
+};
