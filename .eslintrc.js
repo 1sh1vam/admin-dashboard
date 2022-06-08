85720049358df1b890264d9a930a1cccd74e8a1a
1sh1vam <shivam07.mt@gmail.com>
2022-06-08 21:24:24 +0530

    overriding some of the eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,13 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 0,
     'react/function-component-definition': 0,
+    'react/button-has-type': 0,
     'arrow-body-style': 0,
     'object-curly-newline': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'max-len': [
+      2,
+      550,
+    ],
   },
 };
