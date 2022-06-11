bdc4d96e1637f91a7b992dac21386d974ce88e51
1sh1vam <shivam07.mt@gmail.com>
2022-06-11 16:59:02 +0530

    navbar state

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -2,20 +2,25 @@ import { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
-// const initial_state = {
-//   chat: false,
-//   cart: false,
-//   userProfile: false,
-//   notification: false,
-// };
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setClicked] = useState(initialState);
+
+  const handleClick = (clicked) => setClicked({ ...initialState, [clicked]: true });
 
   return (
     <StateContext.Provider value={{
       activeMenu,
       setActiveMenu,
+      isClicked,
+      handleClick,
     }}
     >
       {children}
