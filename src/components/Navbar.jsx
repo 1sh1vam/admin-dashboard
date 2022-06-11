426cf6b61aeb6781fa45fec79e5c3c5d1f2ec80f
1sh1vam <shivam07.mt@gmail.com>
2022-06-11 16:43:14 +0530

    navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
 import { useStateContext } from '../contexts/ContextProvider';
+
+import avatar from '../data/avatar.jpg';
 
 const NavBtn = ({ title, icon, color, dotColor, customFunc }) => (
   <TooltipComponent content={title} position="BottomCenter">
@@ -22,9 +28,26 @@ const NavBtn = ({ title, icon, color, dotColor, customFunc }) => (
 
 function NavBar() {
   const { setActiveMenu } = useStateContext();
+
+  const handleClick = () => {};
+
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavBtn title="Menu" icon={<AiOutlineMenu />} color="blue" customFunc={() => setActiveMenu((prev) => !prev)} />
+
+      <div className="flex">
+        <NavBtn title="Cart" icon={<FiShoppingCart />} color="blue" customFunc={() => handleClick('cart')} />
+        <NavBtn title="Chat" icon={<BsChatLeft />} color="blue" dotColor="#03C9D7" customFunc={() => handleClick('cart')} />
+        <NavBtn title="Notification" icon={<RiNotification3Line />} color="blue" dotColor="#03C9D7" customFunc={() => handleClick('cart')} />
+        <div className="flex text-14 text-gray-400 items-center cursor-pointer gap-2 p-1 hover:bg-light-gray rounded-lg">
+          <img src={avatar} className="w-8 h-8 rounded-full" alt="Avatar" />
+          <p>
+            <span>Hi,</span>
+            <span className="font-bold ml-1">Dhoni</span>
+          </p>
+          <MdKeyboardArrowDown />
+        </div>
+      </div>
     </div>
   );
 }
