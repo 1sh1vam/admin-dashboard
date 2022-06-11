a194fb56e0ad4924be245b29716d81af156b63a4
1sh1vam <shivam07.mt@gmail.com>
2022-06-11 17:27:51 +0530

    close menu when clicking on link if screen size less than 900

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,13 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 function Sidebar() {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, screenSize, setActiveMenu } = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md mt-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray text-md mt-2';
@@ -16,7 +22,7 @@ function Sidebar() {
       {activeMenu && (
         <>
           <div className="flex items-center justify-between">
-            <Link to="/" onClick={() => setActiveMenu(false)} className="flex items-center gap-3 text-xl dark:text-white text-slate-900 font-extrabold tracking-tight ml-3 mt-4">
+            <Link to="/" onClick={handleCloseSidebar} className="flex items-center gap-3 text-xl dark:text-white text-slate-900 font-extrabold tracking-tight ml-3 mt-4">
               <SiShopware />
               <span>Shoppy</span>
             </Link>
@@ -31,7 +37,7 @@ function Sidebar() {
               <div key={item.title}>
                 <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
                 {item.links.map((link) => (
-                  <NavLink to={`/${link.name}`} key={link.name} onClick={() => {}} className={({ isActive }) => (isActive ? activeLink : normalLink)}>
+                  <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSidebar} className={({ isActive }) => (isActive ? activeLink : normalLink)}>
                     {link.icon}
                     <span className="capitalize">{link.name}</span>
                   </NavLink>
