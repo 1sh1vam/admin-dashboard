cc8f96889c3b78022a0cc44b4e70fa8b090957bf
1sh1vam <shivam07.mt@gmail.com>
2022-06-12 15:37:56 +0530

    stacked chart

--- src/components/charts/Stacked.jsx
+++ src/components/charts/Stacked.jsx
@@ -1,6 +1,25 @@
-function Stacked() {
+/* eslint-disable react/no-array-index-key */
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts';
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy';
+
+function Stacked({ width, height }) {
   return (
-    <div>Stacked</div>
+    <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background="#fff"
+      legendSettings={{ background: 'white' }}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={`stack-${index}`} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   );
 }
 
