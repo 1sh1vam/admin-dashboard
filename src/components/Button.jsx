72b2d72e72d39d0dee6a722a4c55fb49bf299815
1sh1vam <shivam07.mt@gmail.com>
2022-06-12 15:24:47 +0530

    fixing button shadow issue

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ function Button({ bgColor, color, text, size, borderRadius, width }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color, borderRadius }}
-      className={`text-${size} p-3 w-${width} hover:drop-shadow-x`}
+      className={`text-${size} p-3 w-${width} hover:drop-shadow-xl`}
     >
       {text}
     </button>
