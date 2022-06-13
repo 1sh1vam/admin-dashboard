c199193963ee88cbd51ccf622d4e92f24ca059d7
1sh1vam <shivam07.mt@gmail.com>
2022-06-13 19:27:52 +0530

    Employees table

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,6 +1,32 @@
+/* eslint-disable react/no-array-index-key */
+import { ColumnsDirective, Inject, ColumnDirective, GridComponent, Search, Page } from '@syncfusion/ej2-react-grids';
+import { Header } from '../components';
+import { employeesData, employeesGrid } from '../data/dummy';
+
 const Employees = () => {
+  const toolbarOptions = ['Search'];
+
+  const editing = { allowDeleting: true, allowEditing: true };
+
   return (
-    <div>Employees</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        id="grid-comp-emplyoees"
+        dataSource={employeesData}
+        width="auto"
+        allowPaging
+        allowSorting
+        pageSettings={{ pageCound: 5 }}
+        editSettings={editing}
+        toolbar={toolbarOptions}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Search, Page]} />
+      </GridComponent>
+    </div>
   );
 };
 
