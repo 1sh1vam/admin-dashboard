c22726cb93fd80682c589e3f6a69424f4f3c8372
1sh1vam <shivam07.mt@gmail.com>
2022-06-13 20:43:01 +0530

    kanban component

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,6 +1,24 @@
+/* eslint-disable react/no-array-index-key */
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
+import { Header } from '../components';
+
+import { kanbanData, kanbanGrid } from '../data/dummy';
+
 const Kanban = () => {
   return (
-    <div>Kanban</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
   );
 };
 
