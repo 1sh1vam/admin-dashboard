ad8edd73fd26dc0f9b755e76dfa30407650d5e42
1sh1vam <shivam07.mt@gmail.com>
2022-06-13 20:36:33 +0530

    calendar page

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,6 +1,58 @@
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+import { Inject, ScheduleComponent, Day, Week, ViewDirective, ViewsDirective, WorkWeek, Month, Agenda, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
+import { useState } from 'react';
+import { Header } from '../components';
+
+import { scheduleData } from '../data/dummy';
+
+const PropertyPane = (props) => <div className="mt-5">{props.children}</div>;
+
 const Calender = () => {
+  const [scheduleObject, setScheduleObject] = useState();
+
+  const change = (args) => {
+    scheduleObject.selectedDate = args.value;
+    scheduleObject.dataBind();
+  };
+
+  const onDragStart = (arg) => {
+    // eslint-disable-next-line no-param-reassign
+    arg.navigation.enable = true;
+  };
+
   return (
-    <div>Calender</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <ScheduleComponent
+        height="650px"
+        ref={(schedule) => setScheduleObject(schedule)}
+        selectedDate={new Date(2021, 0, 10)}
+        eventSettings={{ dataSource: scheduleData }}
+        onDragStart={onDragStart}
+      >
+        <ViewsDirective>
+          {['Day', 'Week', 'WorkWeek', 'Month', 'Agenda'].map((item) => <ViewDirective key={item} option={item} />)}
+        </ViewsDirective>
+        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
+      </ScheduleComponent>
+      <PropertyPane>
+        <table style={{ width: '100%', background: 'white' }}>
+          <tbody>
+            <tr style={{ height: '50px' }}>
+              <td style={{ width: '100%' }}>
+                <DatePickerComponent
+                  value={new Date(2021, 0, 10)}
+                  showClearButton={false}
+                  placeholder="Current Date"
+                  floatLabelType="Always"
+                  change={change}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </PropertyPane>
+    </div>
   );
 };
 
