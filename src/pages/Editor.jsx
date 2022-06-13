06366d32aa954f7c3d8c2cf4181fd4f6aece716f
1sh1vam <shivam07.mt@gmail.com>
2022-06-13 20:48:49 +0530

    rich text editor

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,6 +1,16 @@
+import { Inject, HtmlEditor, RichTextEditorComponent, Toolbar, Image, Link, QuickToolbar } from '@syncfusion/ej2-react-richtexteditor';
+import { Header } from '../components';
+import { EditorData } from '../data/dummy';
+
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
   );
 };
 
