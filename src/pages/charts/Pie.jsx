29e37e831879d8686ab12be20af738d2bccea812
1sh1vam <shivam07.mt@gmail.com>
2022-06-14 20:05:30 +0530

    pie chart page

--- src/pages/charts/Pie.jsx
+++ src/pages/charts/Pie.jsx
@@ -1,6 +1,15 @@
+import Doughnut from '../../components/charts/Pie';
+import ChartsHeader from '../../components/ChartsHeader';
+import { pieChartData } from '../../data/dummy';
+
 const Pie = () => {
   return (
-    <div>Pie</div>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader category="Pie" title="Project Cost Breakdown" />
+      <div className="w-full">
+        <Doughnut id="chart-pie" data={pieChartData} legendVisibility height="full" />
+      </div>
+    </div>
   );
 };
 
