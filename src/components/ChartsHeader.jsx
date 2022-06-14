afbb514b5f9626ec788ef1b57629e4bc3c0b9d14
1sh1vam <shivam07.mt@gmail.com>
2022-06-14 19:27:34 +0530

    Chart Header

--- src/components/ChartsHeader.jsx
+++ src/components/ChartsHeader.jsx
@@ -1,6 +1,12 @@
-function ChartsHeader() {
+function ChartsHeader({ category, title }) {
   return (
-    <div>ChartsHeader</div>
+    <div className="mb-10">
+      <div>
+        <p className="text-gray-400 text-lg">Chart</p>
+        <p className="text-3xl font-extrabold tracking-tighter dark:text-gray-200 text-slate-900">{category}</p>
+      </div>
+      <p className="text-center dark:text-gray-200 text-xl mb-2 mt-3">{title}</p>
+    </div>
   );
 }
 
