0e6ee5fd3d4bd38db3f46c0c694353c7363b217c
1sh1vam <shivam07.mt@gmail.com>
2022-06-15 19:30:25 +0530

    modifying btn to close the right sidebar or popup

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,10 @@
+import { initialState, useStateContext } from '../contexts/ContextProvider';
+
 function Button({ icon, bgColor, color, bgHoverColor, text, size, borderRadius, width }) {
+  const { setClicked } = useStateContext();
   return (
     <button
+      onClick={() => setClicked(initialState)}
       style={{ backgroundColor: bgColor, color, borderRadius }}
       className={`text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
     >
